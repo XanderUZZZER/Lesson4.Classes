5d4421f726e09ee6d10b1ce4d4bcfd4094337037
Alexander <xander_uzzzer@mail.ru>
2017-04-03 20:21:06 +0300

    03041720221

--- Lesson4.Classes/Program.cs
+++ Lesson4.Classes/Program.cs
@@ -11,6 +11,15 @@ namespace Lesson4.Classes
         static void Main(string[] args)
         {
             Console.WriteLine("Lesson 4");
+
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date);
+            date = DateTime.UtcNow;
+            Console.WriteLine(date);
+            Console.WriteLine(DateTime.MinValue);
+
+
+            Console.ReadLine();
         }
     }
 }
